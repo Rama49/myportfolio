c1b5dea2cc9b241556863eb67bf4d11e21f56a85
Rama Fall <rama.fall49@gmail.com>
2023-04-26 23:57:20 +0000

    myprojet

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import {
   MDBCard,
   MDBCardBody,
@@ -10,24 +10,33 @@ import {
 import capture1 from '../images/Capture1.JPG';
 import capture2 from '../images/Capture2.JPG';
 import capture3 from '../images/Capture3.JPG';
+import capture4 from '../images/Capture4.JPG';
+import capture5 from '../images/capture5.JPG';
 // import AOS from 'aos';
+// import 'aos/dist/aos.css';
+import React,{useEffect} from "react";
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 
 
 const Portfolio = () => 
 {
+  useEffect(() => {
+    AOS.init();
+  }, [])
       return (
         <section className='expert'>
-          <h1>Portfolio</h1>
+          <h1 className='mb-5'>Portfolio</h1>
             <div className="row mb-5 justify-content-center">
                 <div className="col-lg-3">
-                <MDBCard>
-    <MDBCardImage src={capture1} position='top' alt='...' className='carte'/>
+              
+  <MDBCard>
+    <MDBCardImage src={capture4} position='top' alt='...'  className='carte'/>
     <MDBCardBody>
-      <MDBCardTitle>Baldesse agro</MDBCardTitle>
+      <MDBCardTitle>meteo</MDBCardTitle>
       <MDBCardText>
-        Dans ce projet nous avons crer un site vitrine qui consistait a exposer ces produits alimentaire et donc a l aider de liquider ces produits tres vites. 
+       Dans ce projet nous avons travailler sur les apis meteo enfin de savoir la temperature des pays et en pouvant changer de ville souhaite
       </MDBCardText>
     </MDBCardBody>
   </MDBCard>
@@ -45,9 +54,11 @@ const Portfolio = () =>
                 </div>
                 <div className="col-lg-3">
                 <MDBCard>
-    <MDBCardImage src={capture3} position='top' alt='...'  className='carte'/>
+                <div data-aos="flip-right">
+                <MDBCardImage src={capture3} position='top' alt='...'  className='carte'/>
+                </div>
     <MDBCardBody>
-      <MDBCardTitle>Card title</MDBCardTitle>
+      <MDBCardTitle>mini projet foot</MDBCardTitle>
       <MDBCardText>
         dans ce projet nous avons creer un systeme qui permettais de suivre l historiques des equipes de football de connaitre leurs nombres de match ceux qu ils ont gagne...
       </MDBCardText>
@@ -58,22 +69,22 @@ const Portfolio = () =>
             <div className="row justify-content-center mb-5">
                 <div className="col-lg-3">
                 <MDBCard>
-    <MDBCardImage src={capture1} position='top' alt='...'  className='carte'/>
+    <MDBCardImage src={capture1} position='top' alt='...' className='carte'/>
     <MDBCardBody>
-      <MDBCardTitle>Card title</MDBCardTitle>
+      <MDBCardTitle>Baldesse agro</MDBCardTitle>
       <MDBCardText>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
+        Dans ce projet nous avons crer un site vitrine qui consistait a exposer ces produits alimentaire et donc a l aider de liquider ces produits tres vites. 
       </MDBCardText>
     </MDBCardBody>
   </MDBCard>
                 </div>
                   <div className="col-lg-3">
                 <MDBCard>
-    <MDBCardImage src={capture2} position='top' alt='...' className='carte' />
+    <MDBCardImage src={capture5} position='top' alt='...' className='carte' />
     <MDBCardBody>
-      <MDBCardTitle>Card title</MDBCardTitle>
+      <MDBCardTitle>Dictionnaire</MDBCardTitle>
       <MDBCardText>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
+        l idee de ce projet consistait a creer un api dictionnaire pour pouvoir rechercher des mots voir ces definition ... des mots
       </MDBCardText>
     </MDBCardBody>
   </MDBCard>
